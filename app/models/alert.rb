9d81a688f43b15b37dd1e0613cde7fa741a5e586
Daniel Drake <dan@reactivated.net>
2015-04-16 12:13:38 -0600

    Store alert type
    Needed to rename type to alert_type as type is a reserved keyword

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -13,7 +13,7 @@
 #  updated_at  :datetime         not null
 #  user_id     :integer
 #  country_id  :integer
-#  type        :string
+#  alert_type  :string
 #
 
 require 'twilio-ruby'
@@ -25,11 +25,11 @@ class Alert < ActiveRecord::Base
   has_many :alert_images
   attr_accessor :address
 
-  validates_presence_of :lat, :lon, :type, :magnitude, :user
+  validates_presence_of :lat, :lon, :alert_type, :magnitude, :user
   validates :lat, inclusion: { in: (-90..90), message: "Invalid latitude value" }
   validates :lon, inclusion: { in: (-180..180), message: "Invalid longitude value" }
   validates :magnitude, inclusion: { in: (1..10), message: "Invalid magnitud value" }
-  validates :type, inclusion: { in: %w(logging fire pest), message: "Invalid alert type" }
+  validates :alert_type, inclusion: { in: %w(logging fire pest), message: "Invalid alert type" }
 
   after_create :send_messages_to_technicians
 
