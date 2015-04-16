92c2e401c0b97a2367aea1ffdd7bd6413b8eb0eb
Daniel Drake <dan@reactivated.net>
2015-04-16 11:44:59 -0600

    Alerts: check that save completes without error

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -6,7 +6,7 @@ class AlertsController < ApplicationController
     # Strong parameters doesn't support hashes with unknown keys, so we handle
     # that separately
     @alert = Alert.new(alert_params.merge(description: params[:alert][:description]))
-    @alert.save
+    @alert.save!
     @alert.alert_images.create!(uploaded_image: params[:alert][:image])
     redirect_to map_path
   end
