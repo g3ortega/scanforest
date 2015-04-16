be04e08f41452951ef3858bc3b36768c05a32e63
Gerardo <geraldavid7@gmail.com>
2015-04-16 14:48:23 -0600

    github authentication strategy

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,15 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  %w{google_oauth2 github}.each do |method|
-    define_method(method) do
-      @user = User.find_for_omniauth(request.env["omniauth.auth"], current_user)
-      if @user.persisted?
-        set_flash_message(:notice, :success, :kind => method.humanize) if is_navigational_format?
-        sign_in_and_redirect @user, :event => :authentication
-      else
-        session["devise.#{method}_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
+  def github
+    @user = User.find_for_omniauth(request.env["omniauth.auth"], current_user)
+    if @user.persisted?
+      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    else
+      session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def google_oauth2
+    @user = User.find_for_omniauth(request.env["omniauth.auth"], current_user)
+    if @user.persisted?
+      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
     end
   end
 
